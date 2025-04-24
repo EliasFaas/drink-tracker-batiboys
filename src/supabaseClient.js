d6b00e2a86eb7141ed2d7310c1373181c6a2ab6e
EliasFaas <faas.henssen@gmail.com>
2025-04-24 18:51:42 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://[JOUW_PROJECT].supabase.co'
-const supabaseAnonKey = '[JOUW_ANON_KEY]'
+const supabaseUrl = 'https://drink-tracker-batiboys.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....' // jouw echte key
 
-export const supabase = createClient(https://drink-tracker-batiboys.vercel.app/,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtodmZlbXJmYnRkcWJkandueHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTI1NjgsImV4cCI6MjA2MTA4ODU2OH0.sl1SZYcViCpz2fz23TNDg6eGSYNQqVPz4EcpWIiqEa8)
+export const supabase = createClient(https://khvfemrfbtdqbdjwnxqd.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtodmZlbXJmYnRkcWJkandueHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTI1NjgsImV4cCI6MjA2MTA4ODU2OH0.sl1SZYcViCpz2fz23TNDg6eGSYNQqVPz4EcpWIiqEa8)
