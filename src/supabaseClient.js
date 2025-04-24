eb5ee6715c0995c7b592233ec84602002cd2c1c1
EliasFaas <faas.henssen@gmail.com>
2025-04-24 18:44:57 +0200

    Create supabaseClient.js

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://[JOUW_PROJECT].supabase.co'
+const supabaseAnonKey = '[JOUW_ANON_KEY]'
+
+export const supabase = createClient(https://drink-tracker-batiboys.vercel.app/,eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtodmZlbXJmYnRkcWJkandueHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTI1NjgsImV4cCI6MjA2MTA4ODU2OH0.sl1SZYcViCpz2fz23TNDg6eGSYNQqVPz4EcpWIiqEa8)
